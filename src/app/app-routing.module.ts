5fc3c8d1e4c3edfd6747e2e60c166bf92856251e
iproman <itechman@yandex.ru>
2019-12-31 11:32:30 +0500

    Add auth.guard service which checks all routes, except reg/log.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,17 +8,18 @@ import {EditClientComponent} from './components/edit-client/edit-client.componen
 import {ClientDetailsComponent} from './components/client-details/client-details.component';
 import {SettingsComponent} from './components/settings/settings.component';
 import {NotFoundComponent} from './components/not-found/not-found.component';
+import {AuthGuard} from './guards/auth.guard';
 
 const routes: Routes = [
-  {path: '', component: DashboardComponent},
+  {path: '', component: DashboardComponent, canActivate: [AuthGuard]},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'client/add', component: AddClientComponent},
-  {path: 'client/edit', component: EditClientComponent},
-  {path: 'client/edit/:id', component: EditClientComponent}
+  {path: 'client/add', component: AddClientComponent, canActivate: [AuthGuard]},
+  {path: 'client/edit', component: EditClientComponent, canActivate: [AuthGuard]},
+  {path: 'client/edit/:id', component: EditClientComponent, canActivate: [AuthGuard]}
   ,
-  {path: 'client/:id', component: ClientDetailsComponent},
-  {path: 'settings', component: SettingsComponent},
+  {path: 'client/:id', component: ClientDetailsComponent, canActivate: [AuthGuard]},
+  {path: 'settings', component: SettingsComponent, canActivate: [AuthGuard]},
   {path: '**', component: NotFoundComponent},
 ];
 
@@ -28,7 +29,9 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes)
   ],
-  declarations: [],
+  providers: [
+    AuthGuard
+  ]
 })
 export class AppRoutingModule {
 }
