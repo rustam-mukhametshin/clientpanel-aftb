5bb88d5cb9219f8e4765226b4bdc5a832eb25568
iproman <itechman@yandex.ru>
2019-12-31 11:17:38 +0500

    Quick links in navigation, check auth of user.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {FlashMessagesService} from 'angular2-flash-messages';
+import {AuthService} from '../../services/auth.service';
+import {AngularFireAuth} from '@angular/fire/auth';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  isLoggedIn?: boolean;
+  loggedInUser: string;
+  showRegister: boolean;
+
+  constructor(
+    private authService: AuthService,
+    private afAuth: AngularFireAuth,
+    private router: Router,
+    private flashMessage: FlashMessagesService
+  ) {
+  }
 
   ngOnInit() {
+    this.authService.getAuth().subscribe(auth => {
+      if (auth) {
+        this.isLoggedIn = true;
+        this.loggedInUser = auth.email;
+      } else {
+        this.isLoggedIn = false;
+      }
+    });
   }
 
 }
