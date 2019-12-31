a842e5e85c6700cfed53f89e74f83f846f85f972
iproman <itechman@yandex.ru>
2019-12-31 09:00:39 +0500

    Add AuthService with login method.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { ClientsComponent } from './components/clients/clients.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { AddClientComponent } from './components/add-client/add-client.component';
-import { EditClientComponent } from './components/edit-client/edit-client.component';
-import { ClientDetailsComponent } from './components/client-details/client-details.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { SettingsComponent } from './components/settings/settings.component';
-import {NotFoundComponent} from './components/not-found/not-found.component';
-import { AppRoutingModule } from './app-routing.module';
+
 import {AngularFireModule} from '@angular/fire';
-import {environment} from '../environments/environment';
 import {AngularFirestoreModule} from '@angular/fire/firestore';
 import {AngularFireAuthModule} from '@angular/fire/auth';
+
 import {FlashMessagesModule} from 'angular2-flash-messages';
+
+import {environment} from '../environments/environment';
+
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {ClientsComponent} from './components/clients/clients.component';
+import {SidebarComponent} from './components/sidebar/sidebar.component';
+import {AddClientComponent} from './components/add-client/add-client.component';
+import {EditClientComponent} from './components/edit-client/edit-client.component';
+import {ClientDetailsComponent} from './components/client-details/client-details.component';
+import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
+import {SettingsComponent} from './components/settings/settings.component';
+import {NotFoundComponent} from './components/not-found/not-found.component';
+
+import {ClientService} from './services/client.service';
+import {AuthService} from './services/auth.service';
+
 
 @NgModule({
   declarations: [
@@ -44,7 +53,11 @@ import {FlashMessagesModule} from 'angular2-flash-messages';
     FormsModule,
     FlashMessagesModule.forRoot(),
   ],
-  providers: [],
+  providers: [
+    ClientService,
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
